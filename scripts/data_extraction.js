020472cdf515d0e2d36f4ba72f33c3d0fc2fef9e
franklin050187 <cat050187@gmail.com>
2024-08-26 11:06:15 +0200

    lint + remove duplicate functions

--- scripts/data_extraction.js
+++ scripts/data_extraction.js
@@ -1,76 +1,85 @@
 //This file is for functions that extract some kind of information.
 
 function getPartData(part) {
-    data = []
-    for (const entry of part_toggles) {
-        const key = entry.Key || {};
-        if (part.Location[0] == key[0].Location[0] && part.Location[1] == key[0].Location[1]) {
-            data.push(entry);
-        }
-    }
-    return data;
+	data = [];
+	for (const entry of part_toggles) {
+		const key = entry.Key || {};
+		if (
+			part.Location[0] === key[0].Location[0] &&
+			part.Location[1] === key[0].Location[1]
+		) {
+			data.push(entry);
+		}
+	}
+	return data;
 }
 
 function getShipDataMap() {
-    let filtered_ship_data = new Map();
+	const filtered_ship_data = new Map();
 
-    filtered_ship_data.set("Author", shipdata.Author);
-    filtered_ship_data.set("FlightDirection", shipdata.FlightDirection);
-    filtered_ship_data.set("Name", shipdata.Name);
-    filtered_ship_data.set("DefaultAttackFollowAngle", shipdata.DefaultAttackFollowAngle);
-    filtered_ship_data.set("DefaultAttackRadius", shipdata.DefaultAttackRadius);
-    filtered_ship_data.set("DefaultAttackRotation", shipdata.DefaultAttackRotation);
+	filtered_ship_data.set("Author", shipdata.Author);
+	filtered_ship_data.set("FlightDirection", shipdata.FlightDirection);
+	filtered_ship_data.set("Name", shipdata.Name);
+	filtered_ship_data.set(
+		"DefaultAttackFollowAngle",
+		shipdata.DefaultAttackFollowAngle,
+	);
+	filtered_ship_data.set("DefaultAttackRadius", shipdata.DefaultAttackRadius);
+	filtered_ship_data.set(
+		"DefaultAttackRotation",
+		shipdata.DefaultAttackRotation,
+	);
 
-    return filtered_ship_data;
+	return filtered_ship_data;
 }
 
 function getPartToggles(part) {
-    toggles = [];
-    for (toggle of part_toggles) {
-        if (toggle.Key[0].ID == part["ID"]) {
-            toggles.push(toggle);
-        }
-    }
-    return toggles;
+	toggles = [];
+	for (toggle of part_toggles) {
+		if (toggle.Key[0].ID === part.ID) {
+			toggles.push(toggle);
+		}
+	}
+	return toggles;
 }
 
 function getParts(id = null, category = null) {
-    parts = []
-    for (sprite of sprites) {
-        if ((sprite.ID == id || id == null) && (category == null || category == spriteData[sprite["ID"]].category)) {
-            parts.push(sprite)
-        }
-    }
-    return parts
+	parts = [];
+	for (sprite of sprites) {
+		if (
+			(sprite.ID === id || id == null) &&
+			(category == null || category === spriteData[sprite.ID].category)
+		) {
+			parts.push(sprite);
+		}
+	}
+	return parts;
 }
 
 function getPartDataMap(part) {
-    let part_data = new Map()
-    let raw_data = getPartData(part)
+	const part_data = new Map();
+	const raw_data = getPartData(part);
 
-    for (const entry of raw_data) {
-        const key = entry.Key || {};
+	for (const entry of raw_data) {
+		const key = entry.Key || {};
 
-        // Power toggle (1 is on 0 is off)
-        if (key.includes("on_off")) {
-            part_data.set("on_off", entry.Value);
-        }
+		// Power toggle (1 is on 0 is off)
+		if (key.includes("on_off")) {
+			part_data.set("on_off", entry.Value);
+		}
 
-        //fire mode (hold fire:2^32-1, fire at target: 1, fire at will: 0, autofire: 2)
-        if (key.includes("fire_mode")) {
-            part_data.set("fire_mode", entry.Value);
-        }
+		//fire mode (hold fire:2^32-1, fire at target: 1, fire at will: 0, autofire: 2)
+		if (key.includes("fire_mode")) {
+			part_data.set("fire_mode", entry.Value);
+		}
 
-        //missile type (3 is mine, 1 is emp, 2 is nuke, 0 is he)
-        if (key.includes("missile_type")) {
-            part_data.set("missile_type", entry.Value);
-        }
-
-    }
-    return part_data
+		//missile type (3 is mine, 1 is emp, 2 is nuke, 0 is he)
+		if (key.includes("missile_type")) {
+			part_data.set("missile_type", entry.Value);
+		}
+	}
+	return part_data;
 }
-
-
 
 //Unfinished cause it seemed annoying to do and its not very important right now
 /*function getCGsMagsMap() {
--- scripts/warnings.js
+++ scripts/warnings.js
@@ -2,49 +2,71 @@
 //A warning should be of the form: {id: "id_string", message: message_string, locations: [Locations_array], tags: tag_string_array}
 
 const warning_function_list = [
-    function feWarning() {
-        let fes = getParts("cosmoteer.fire_extinguisher")
-        if (fes.length==0) {
-            return {id: "no_fire_extinguisher", message: "Your ship does not contain any fire extinguishers", locations: [], tags: []}
-        }
-    },
-    function airlockWarning() {
-        let airlocks = getParts("cosmoteer.airlock")
-        if (airlocks.length==0) {
-            return {id: "no_airlock",  message: "Your ship does not contain any airlocks", locations: [], tags: []}
-        }
-    },
-    function cpWarning() {
-        if (getShipCommandCost() > getShipCommandPoints()) {
-            return {id: "not_enough_command_points",  message: "Your ship does not have enough command points", locations: [], tags: []}
-        }
-    },
-    function connectionWarning() {
-        const graph = getShipPartConnectionGraph(sprites)
-        const partition = getConnectedComponents(graph[0],graph[1])
-        if (partition.length > 1) {
-            return {id: "not_connected",  message: "Your ship is not connected", locations: [], tags: ["computationally_expensive"]}
-        }
-    }
-]
+	function feWarning() {
+		const fes = getParts("cosmoteer.fire_extinguisher");
+		if (fes.length === 0) {
+			return {
+				id: "no_fire_extinguisher",
+				message: "Your ship does not contain any fire extinguishers",
+				locations: [],
+				tags: [],
+			};
+		}
+	},
+	function airlockWarning() {
+		const airlocks = getParts("cosmoteer.airlock");
+		if (airlocks.length === 0) {
+			return {
+				id: "no_airlock",
+				message: "Your ship does not contain any airlocks",
+				locations: [],
+				tags: [],
+			};
+		}
+	},
+	function cpWarning() {
+		if (getShipCommandCost() > getShipCommandPoints()) {
+			return {
+				id: "not_enough_command_points",
+				message: "Your ship does not have enough command points",
+				locations: [],
+				tags: [],
+			};
+		}
+	},
+	function connectionWarning() {
+		const graph = getShipPartConnectionGraph(sprites);
+		const partition = getConnectedComponents(graph[0], graph[1]);
+		if (partition.length > 1) {
+			return {
+				id: "not_connected",
+				message: "Your ship is not connected",
+				locations: [],
+				tags: ["computationally_expensive"],
+			};
+		}
+	},
+];
 
-function getWarnings(bool) {
-    let warnings = []
-    for (let warning of warning_function_list) {
-        if (warning() !== undefined && (!warning.tags.includes("computationally_expensive") || bool)) {
-            warnings.push(warning())
-        }
-    }
-    return warnings
+function getWarnings(includeExpensive) {
+	const warnings = [];
+	for (const warningFunction of warning_function_list) {
+		const warning = warningFunction();
+		if (
+			warning !== undefined &&
+			(!warning.tags.includes("computationally_expensive") || includeExpensive)
+		) {
+			warnings.push(warning);
+		}
+	}
+	return warnings;
 }
 
 function containsWarning(warnings, warning_id) {
-    for (let warning of warnings) {
-        if (warning.id == warning_id) {
-            return true
-        }
-    }
-    return false
+	for (const warning of warnings) {
+		if (warning.id === warning_id) {
+			return true;
+		}
+	}
+	return false;
 }
-
-
